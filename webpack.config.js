45f4fffe49f989cce8aaba09f22f36dcdc355b20
Zoltan Fischer <zoli_fischer@yahoo.com>
2018-12-05 21:46:05 +0100

    v1.0.1

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ const config = {
         path: path.resolve(__dirname, 'lib'),
         filename: '[name].js',
         library: libraryName,
-        libraryTarget: 'commonjs',
+        libraryTarget: 'commonjs2',
     },
     module: {
         rules: [
